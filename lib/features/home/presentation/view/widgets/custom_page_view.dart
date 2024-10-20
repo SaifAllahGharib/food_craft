8e4d26b535984ae7935e002d4699ccbe16f72504
SaifAllahGharib <saifallahgharib@gmail.com>
2024-10-20 15:10:58 +0300

    create page indicator

--- lib/features/home/presentation/view/widgets/custom_page_view.dart
+++ lib/features/home/presentation/view/widgets/custom_page_view.dart
@@ -2,45 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:food_craft/core/utils/functions/height.dart';
 import 'package:food_craft/features/home/presentation/view/widgets/page_view_item.dart';
 
-class CustomPageView extends StatefulWidget {
-  const CustomPageView({super.key});
+class CustomPageView extends StatelessWidget {
+  final PageController pageController;
+  final double currentPage;
+  final int countOfPage;
 
-  @override
-  State<CustomPageView> createState() => _CustomPageViewState();
-}
-
-class _CustomPageViewState extends State<CustomPageView> {
-  late PageController _pageController;
-  double _currentPage = 2;
-
-  @override
-  void initState() {
-    _pageController = PageController(viewportFraction: 0.85, initialPage: 2)
-      ..addListener(
-        () {
-          _currentPage = _pageController.page!;
-          setState(() {});
-        },
-      );
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
+  const CustomPageView({
+    super.key,
+    required this.pageController,
+    required this.currentPage,
+    required this.countOfPage,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: getHeight(context) * 0.36,
       child: PageView.builder(
-        controller: _pageController,
-        itemCount: 5,
+        controller: pageController,
+        itemCount: countOfPage,
         itemBuilder: (context, index) => PageViewItem(
-          pageController: _pageController,
-          currentPage: _currentPage,
+          pageController: pageController,
+          currentPage: currentPage,
           index: index,
         ),
       ),
--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:food_craft/constant.dart';
+import 'package:food_craft/core/utils/colors.dart';
 import 'package:food_craft/core/utils/functions/height.dart';
+import 'package:food_craft/core/utils/functions/width.dart';
 import 'package:food_craft/features/home/presentation/view/widgets/custom_app_bar_home_view.dart';
 import 'package:food_craft/features/home/presentation/view/widgets/custom_page_view.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
 
   @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  final int countOfPage = 5;
+  late PageController _pageController;
+  double _currentPage = 2;
+
+  @override
+  void initState() {
+    initAndListenPageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final double dotSize = getWidth(context) * 0.03;
     return Container(
       margin: const EdgeInsets.only(top: 10, bottom: 15),
       padding:
@@ -17,9 +42,46 @@ class HomeViewBody extends StatelessWidget {
         children: [
           const CustomAppBarHomeView(),
           SizedBox(height: getHeight(context) * 0.05),
-          const CustomPageView(),
+          CustomPageView(
+            pageController: _pageController,
+            currentPage: _currentPage,
+            countOfPage: countOfPage,
+          ),
+          SizedBox(height: getHeight(context) * 0.025),
+          SmoothPageIndicator(
+            controller: _pageController,
+            count: countOfPage,
+            effect: SwapEffect(
+              activeDotColor: AppColors.primaryColor,
+              spacing: 13,
+              dotWidth: dotSize,
+              dotHeight: dotSize,
+              type: SwapType.yRotation,
+            ),
+            onDotClicked: (index) {
+              onClickInDot(index);
+            },
+          ),
         ],
       ),
+    );
+  }
+
+  void initAndListenPageController() {
+    _pageController = PageController(viewportFraction: 0.85, initialPage: 2)
+      ..addListener(
+        () {
+          _currentPage = _pageController.page!;
+          setState(() {});
+        },
+      );
+  }
+
+  void onClickInDot(int index) {
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
     );
   }
 }
